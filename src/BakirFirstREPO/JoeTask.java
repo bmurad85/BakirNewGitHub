475ce5dcff92b89d46e36a1126f48d6a5f3acc32
BakirFirstREPO <108557728+bmurad85@users.noreply.github.com>
2022-07-07 20:48:52 -0500

    This is my SMS

---
+++ src/BakirFirstREPO/JoeTask.java
@@ -0,0 +1,9 @@
+package BakirFirstREPO;
+
+public class JoeTask {
+    public static void main(String[] args) {
+        System.out.println("Joe Version One");
+
+
+    }
+}
---
+++ src/BakirFirstREPO/Test.java
@@ -0,0 +1,7 @@
+package BakirFirstREPO;
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
