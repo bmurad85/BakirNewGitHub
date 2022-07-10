0ebbbb52a4364ffaa98cbc193d5fe815b63f589c
BakirFirstREPO <108557728+bmurad85@users.noreply.github.com>
2022-07-10 17:01:17 -0500

    this is SMS

--- src/BakirFirstREPO/JoeTask.java
+++ src/BakirFirstREPO/JoeTask.java
@@ -3,7 +3,7 @@ package BakirFirstREPO;
 public class JoeTask {
     public static void main(String[] args) {
         System.out.println("Joe Version One");
-
-
+        // It is just a new note
+        System.out.println("Test with group");
     }
 }
